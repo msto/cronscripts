d9bd61491355f099aea8de15ff14d845be7f8265
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-07-03 11:57:35 -0400

    run ncdu instead of du

---
+++ ncdu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# du.sh
+# Copyright (C) 2015 Matthew Stone <mstone5@mgh.harvard.edu>
+#
+# Distributed under terms of the MIT license.
+#
+PATH=/bin:/usr/bin:/apps/lab/miket/ncdu/1.12/bin
+
+dir=$1
+
+# Initialize logging if necessary
+mkdir -p ${dir}/du_logs
+mkdir -p ${dir}/du_logs/daily
+mkdir -p ${dir}/du_logs/weekly
+mkdir -p ${dir}/du_logs/monthly
+
+# Log disk usage daily
+currdate=$(date +"%Y%m%d")
+NCDU_LOG=${dir}/du_logs/daily/${currdate}.ncdu.gz
+ncdu -x -r -0 --si -o - $dir | gzip -c > $NCDU_LOG
+
+# Log weekly disk usage every Saturday
+if [ $(date +'%u') -eq 6 ]; then
+    cp ${NCDU_LOG} ${dir}/du_logs/weekly/
+fi
+
+# Log disk usage on the first of every month
+if [ $(date +'%e') -eq 1 ]; then
+    cp ${NCDU_LOG} ${dir}/du_logs/monthly/
+fi
+
+# Clean log directories.
+# Keep daily logs for a week and weekly logs for 90 days
+# Keep daily logs for 30 days and weekly logs for a year
+tmpwatch 30d ${dir}/du_logs/daily
+tmpwatch 365d ${dir}/du_logs/weekly
